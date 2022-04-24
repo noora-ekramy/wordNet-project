f509b24195fb99cbc0ea9a0f2a1c993b27dcd52f
noora-ekramy <nora.ekramy11@gmail.com>
2022-04-24 23:37:04 +0200

    Mapping Nouns To Synsets IDs completed

--- MappingNounToSynsetsIDs.cs
+++ MappingNounToSynsetsIDs.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.IO;
+using System.Diagnostics;
 namespace wordNet_project
 {
     class MappingNounToSynsetsIDs
     {
-        
+        public static List<int> Maping_Noun_To_SynsetsIDs( Dictionary<string, List<int>> Words , string Word)
+        {
+            
+            if (Words.ContainsKey(Word))
+                return Words[Word];
+            else
+                return new List<int> { };
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,13 +7,18 @@ namespace wordNet_project
     {
         static void Main(string[] args)
         {
-            #region teating Graph constructors
+            #region testing Graph constructors
             Console.WriteLine("Hello World!");
             List<string> lines = new List<string>();
             lines = ReadFromFile.Read_From_File("D:\\level 3\\semester 6\\algorithm\\project\\OneDrive_2022-04-23\\[5] WordNet Semantics\\Testcases\\Testcases\\Sample\\Case1\\Input\\1synsets.txt");
             List<string> lines1 = new List<string>();
             lines1 = ReadFromFile.Read_From_File("D:\\level 3\\semester 6\\algorithm\\project\\OneDrive_2022-04-23\\[5] WordNet Semantics\\Testcases\\Testcases\\Sample\\Case1\\Input\\2hypernyms.txt");
-            Graph_construction New_Graph = new Graph_construction(lines, lines1);  
+            Graph_construction New_Graph = new Graph_construction(lines, lines1);
+            #endregion
+
+            #region testing Maping Nouns to IDs
+            List<int> Maping_Answer = new List<int>();
+            Maping_Answer = MappingNounToSynsetsIDs.Maping_Noun_To_SynsetsIDs( New_Graph.Words, "a");
             #endregion
 
 
