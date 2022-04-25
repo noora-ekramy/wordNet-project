dc1dfb6f44537fe4ab0ddaac19e8ba2dad403e27
Nourhan Mahmoud Ahmed <nourhanmahmoud623@gmail.com>
2022-04-25 16:41:26 +0200

    Modification in Mapping_SynsetsID_To_Nouns class & add testing region in program.cs

--- MappingSynsetsIDToNouns.cs
+++ MappingSynsetsIDToNouns.cs
@@ -6,10 +6,10 @@ namespace wordNet_project
 {
     class MappingSynsetsIDToNouns
     {
-        public static List<string> Mapping_SynsetsID_To_Nouns(Dictionary<int, List<string>> SynsetsID, int SynsetID)
+        public static List<string> Mapping_SynsetsID_To_Nouns(List<List<string>> SynsetsID, int SynsetID)
         {
-
-            if (SynsetsID.ContainsKey(SynsetID))
+            
+            if (SynsetsID[SynsetID] != null)
                 return SynsetsID[SynsetID];
             else
                 return new List<string> { };
--- Program.cs
+++ Program.cs
@@ -10,9 +10,9 @@ namespace wordNet_project
             #region testing Graph constructors
             Console.WriteLine("Hello World!");
             List<string> lines = new List<string>();
-            lines = ReadFromFile.Read_From_File("D:\\level 3\\semester 6\\algorithm\\project\\OneDrive_2022-04-23\\[5] WordNet Semantics\\Testcases\\Testcases\\Sample\\Case1\\Input\\1synsets.txt");
+            lines = ReadFromFile.Read_From_File("C:\\Users\\User\\OneDrive\\Desktop\\Testcases\\Sample\\Case1\\Input\\1synsets.txt");
             List<string> lines1 = new List<string>();
-            lines1 = ReadFromFile.Read_From_File("D:\\level 3\\semester 6\\algorithm\\project\\OneDrive_2022-04-23\\[5] WordNet Semantics\\Testcases\\Testcases\\Sample\\Case1\\Input\\2hypernyms.txt");
+            lines1 = ReadFromFile.Read_From_File("C:\\Users\\User\\OneDrive\\Desktop\\Testcases\\Sample\\Case1\\Input\\2hypernyms.txt");
             Graph_construction New_Graph = new Graph_construction(lines, lines1);
             #endregion
 
@@ -21,6 +21,10 @@ namespace wordNet_project
             Maping_Answer = MappingNounToSynsetsIDs.Maping_Noun_To_SynsetsIDs( New_Graph.Words, "a");
             #endregion
 
+            #region testing Mapping SynsetsID To Nouns
+            List<string> Maping_AnswerFor1 = new List<string>();
+            Maping_AnswerFor1 = MappingSynsetsIDToNouns.Mapping_SynsetsID_To_Nouns( New_Graph.Synsets, 1);
+            #endregion
 
 
 
