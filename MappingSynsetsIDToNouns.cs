aa3927f5d39fb07a57fd17e159ca0b2d527ecc08
Nourhan Mahmoud Ahmed <nourhanmahmoud623@gmail.com>
2022-04-25 02:01:43 +0200

    MappingSynsetsIDToNouns

---
+++ MappingSynsetsIDToNouns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wordNet_project
+{
+    class MappingSynsetsIDToNouns
+    {
+        public static List<string> Mapping_SynsetsID_To_Nouns(Dictionary<int, List<string>> SynsetsID, int SynsetID)
+        {
+
+            if (SynsetsID.ContainsKey(SynsetID))
+                return SynsetsID[SynsetID];
+            else
+                return new List<string> { };
+        }
+    }
+}
