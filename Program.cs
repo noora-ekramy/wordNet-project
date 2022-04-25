358c5dca9b68454887ef23d1e0faa573d0c262d1
Motaz <44081702+MotazAh@users.noreply.github.com>
2022-04-25 07:42:48 +0200

    Added TestCases

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Reflection;
 namespace wordNet_project
 {
     class Program
@@ -9,10 +10,11 @@ namespace wordNet_project
         {
             #region testing Graph constructors
             Console.WriteLine("Hello World!");
+
             List<string> lines = new List<string>();
-            lines = ReadFromFile.Read_From_File("D:\\level 3\\semester 6\\algorithm\\project\\OneDrive_2022-04-23\\[5] WordNet Semantics\\Testcases\\Testcases\\Sample\\Case1\\Input\\1synsets.txt");
+            lines = ReadFromFile.Read_From_File("..\\..\\..\\Testcases\\Sample\\Case1\\Input\\1synsets.txt");
             List<string> lines1 = new List<string>();
-            lines1 = ReadFromFile.Read_From_File("D:\\level 3\\semester 6\\algorithm\\project\\OneDrive_2022-04-23\\[5] WordNet Semantics\\Testcases\\Testcases\\Sample\\Case1\\Input\\2hypernyms.txt");
+            lines1 = ReadFromFile.Read_From_File("..\\..\\..\\Testcases\\Sample\\Case1\\Input\\2hypernyms.txt");
             Graph_construction New_Graph = new Graph_construction(lines, lines1);
             #endregion
 
