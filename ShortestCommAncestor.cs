53022033bb914a0c7375dc2755af3bfc92ef94d2
Motaz <44081702+MotazAh@users.noreply.github.com>
2022-05-16 22:11:32 +0200

    Update ShortestCommAncestor.cs

--- ShortestCommAncestor.cs
+++ ShortestCommAncestor.cs
@@ -7,16 +7,16 @@ namespace wordNet_project
 {
     class ShortestCommAncestor
     {
-        private int shortestAncestorID = -1;
-        public int shortestLength = int.MaxValue;
-        public List<int> shortestPath = new List<int>();
+        private int shortestAncestorID;
+        public int shortestLength;
+        public List<int> shortestPath;
         public List<List<int>> Graph;
-        private List<List<int>> pathsOf1 = new List<List<int>>();
-        private List<List<int>> pathsOf2 = new List<List<int>>();
-        private List<List<int>> commonPaths = new List<List<int>>();
-        private List<int> commonParents = new List<int>();
+        private List<List<int>> pathsOf1;
+        private List<List<int>> pathsOf2;
+        private List<List<int>> commonPaths;
+        private List<int> commonParents;
         private Color[] status;
-        private bool isDirectlyConnected = false; // For if the two input IDs are direct neighbours
+        private bool isDirectlyConnected; // For if the two input IDs are direct neighbours
 
         enum Color
         {
@@ -35,6 +35,14 @@ namespace wordNet_project
         // Uses DFS (not optimally)
         public int getSCA(int ID1, int ID2)
         {
+            isDirectlyConnected = false;
+            shortestAncestorID = -1;
+            shortestLength = int.MaxValue;
+            shortestPath = new List<int>();
+            commonParents = new List<int>();
+            pathsOf1 = new List<List<int>>();
+            commonPaths = new List<List<int>>();
+            pathsOf2 = new List<List<int>>();
             status = new Color[Graph.Count];
             // Return length 0 as both input nodes are the same one
             if (ID1 == ID2)
@@ -51,12 +59,15 @@ namespace wordNet_project
             // Explore all vertices
             for (int v = 0; v < Graph.Count; v++)
             {
+
+                //if (status[v] == Color.WHITE)
+                //    VisitVertex(ID1, ID2, v, -1);
+                if (v == ID1 || v == ID2)
+                    VisitVertexA(ID1, ID2, v, v);
                 if (isDirectlyConnected) // End as shortest common ancestor is found
                     return shortestAncestorID;
-                if (status[v] == Color.WHITE)
-                    VisitVertex(ID1, ID2, v, -1);
             }
-            
+
             List<int> shortestPath1 = new List<int>();
             List<int> shortestPath2 = new List<int>();
             int minPathLen = int.MaxValue;
@@ -83,7 +94,7 @@ namespace wordNet_project
                         }
                     }
                 }
-                
+
                 minPathLen = int.MaxValue;
 
                 // Get shortest path for a common parent in paths of 2
@@ -112,8 +123,10 @@ namespace wordNet_project
                 minPathLen = int.MaxValue;
                 commonPaths.Add(new List<int>(shortestPath1.Count + shortestPath2.Count));
                 shortestPath2.Reverse();
+
                 commonPaths[commonPaths.Count - 1].AddRange(shortestPath1);
                 commonPaths[commonPaths.Count - 1].AddRange(shortestPath2);
+
                 shortestPath2.Reverse();
             }
 
@@ -121,12 +134,14 @@ namespace wordNet_project
             // Find the shortest path in common paths
             foreach (List<int> path in commonPaths)
             {
+                if (path[path.Count - 1] == path[path.Count - 2])
+                    path.RemoveAt(path.Count - 1);
                 if (path.Count < minPathLen)
                 {
                     minPathLen = path.Count;
                     shortestPath = path;
-                    shortestLength = shortestPath.Count - 1;
                     shortestAncestorID = commonParents[count];
+                    shortestLength = shortestPath.Count - 1;
                 }
                 count++;
             }
@@ -136,13 +151,13 @@ namespace wordNet_project
         int pathLength1 = 0;
         int pathLength2 = 0;
         // Recursive function to explore vertices
-        void VisitVertex(int ID1, int ID2, int v, int foundID)
+        void VisitVertexA(int ID1, int ID2, int v, int searchID)
         {
-            if (isDirectlyConnected || status[v] == Color.RED) // Don't explore any more vertices
+            if (isDirectlyConnected) // Don't explore any more vertices
                 return;
 
             // Adds current vertex to current ID1 path
-            if (foundID == ID1)
+            if (searchID == ID1)
             {
                 pathLength1++;
                 if (pathsOf1.Count != 0)
@@ -152,16 +167,14 @@ namespace wordNet_project
                 if (status[v] == Color.BLUE) // Checks if vertex is a common ancestor
                 {
                     if (!commonParents.Contains(v))
-                    {
                         commonParents.Add(v);
-                    }
                     status[v] = Color.BLACK;
                 }
                 else
                     status[v] = Color.GREEN;
             }
             // Adds current vertex to current ID2 path
-            if (foundID == ID2)
+            if (searchID == ID2)
             {
                 pathLength2++;
                 if (pathsOf2.Count != 0)
@@ -171,9 +184,7 @@ namespace wordNet_project
                 if (status[v] == Color.GREEN || status[v] == Color.BLACK) // Checks if vertex is a common ancestor
                 {
                     if (!commonParents.Contains(v))
-                    {
                         commonParents.Add(v);
-                    }
                     status[v] = Color.BLACK;
                 }
                 else
@@ -184,61 +195,38 @@ namespace wordNet_project
             {
                 if (v == ID1) // If exploring child vertex of ID1 then add new path for ID2
                 {
-                    status[v] = Color.RED; // Changes color to not revisit
+                    status[v] = Color.GREEN; // Changes color to not revisit
                     List<int> path = new List<int>();
                     path.Add(v);
-                    foundID = ID1;
                     pathsOf1.Add(path);
-                }
-                if (v == ID2) // If exploring child vertex of ID2 then add new path for ID2
-                {
-                    status[v] = Color.RED;  // Changes color to not revisit
-                    List<int> path = new List<int>();
-                    path.Add(v);
-                    foundID = ID2;
-                    pathsOf2.Add(path);
-                }
-                if (foundID == ID1) // Checks if the node found is node ID1
-                {
-                    if (vertex == ID2) // If the vertex is ID2, then ID2 itself is the shortest ancestor of itself and ID1
-                    {
-                        isDirectlyConnected = true;
-                        shortestAncestorID = ID1;
-                        shortestPath.Add(ID2);
-                        shortestPath.Add(ID1);
-                        shortestLength = 1;
-                        return;
-                    }
-                    if (pathsOf1[pathsOf1.Count - 1].Count > pathLength1 + 1)
-                    {
-                        List<int> path = pathsOf1[pathsOf1.Count - 1];
-                        while (path.Count > pathLength1 + 1)
-                            path.RemoveAt(path.Count - 1);
-                        pathsOf1.Add(path);
-                    }
-                }
-                if (foundID == ID2) // If Second vertex matches input vertex, check if it is the minimum distance
-                {
-                    if (vertex == ID1) // If the vertex is ID1, then ID1 itself is the shortest ancestor of itself and ID2
+                    if (vertex == ID2)
                     {
                         isDirectlyConnected = true;
                         shortestAncestorID = ID2;
+                        shortestPath.Add(ID2);
+                        shortestPath.Add(ID1);
+                        shortestLength = 1;
+                        return;
+                    }
+                }
+                if (v == ID2) // If exploring child vertex of ID2 then add new path for ID2
+                {
+                    status[v] = Color.BLUE;  // Changes color to not revisit
+                    List<int> path = new List<int>();
+                    path.Add(v);
+                    pathsOf2.Add(path);
+                    if (vertex == ID1) // If the vertex is ID1, then ID1 itself is the shortest ancestor of itself and ID2
+                    {
+                        isDirectlyConnected = true;
+                        shortestAncestorID = ID1;
                         shortestPath.Add(ID1);
                         shortestPath.Add(ID2);
                         shortestLength = 1;
                         return;
                     }
-                    if (pathsOf2[pathsOf2.Count - 1].Count > pathLength2 + 1)
-                    {
-                        List<int> path = new List<int>(pathLength2 + 1);
-                        while (path.Count > pathLength2 + 1)
-                            path.RemoveAt(path.Count - 1);
-                        pathsOf2.Add(path);
-                    }
                 }
-
                 // Explore child vertex
-                VisitVertex(ID1, ID2, vertex, foundID);
+                VisitVertexA(ID1, ID2, vertex, searchID);
             }
             if (pathLength1 > 0)
                 pathLength1--;
