e61545f0b708a3d95229fe03cc070d317d10f9d8
Nora Ekramy <nora.ekramy11@gmail.com>
2022-05-17 06:31:58 +0200

    Complet testcases not Working

--- TestWordNetProject.cs
+++ TestWordNetProject.cs
@@ -346,13 +346,13 @@ namespace wordNet_project
         public static void CompletTest_using_SCA()
         {
             Console.WriteLine("\n\n\nRunning Complet testcases................ ");
+            Console.WriteLine("\n\n\nRunning Complet testcases................ ");
             #region Small
             #region case1_100_100
             // bool flag = false;
             //----------------------------------------------------------
             Console.WriteLine("Running case1_100_100.................");
             //loading data from files
-            //---------------------------------------------------------
             List<string> synsets = new List<string>();
             synsets = ReadFromFile.Read_From_File(@"\Testcases\Complete\1-Small\Case1_100_100\Input\1synsets.txt");
 
@@ -371,7 +371,7 @@ namespace wordNet_project
             List<string> Output2 = new List<string>();
             Output2 = ReadFromFile.Read_From_File(@"\Testcases\\Complete\1-Small\Case1_100_100\\Output\\Output2.txt");
             //----------------------------------------------------------
-            bool flag = Testing(true, synsets, hypernyms, relationQueries, outcastQueries, Output1, Output2, "case1_100_100");
+            bool flag = Testing_complet(true, synsets, hypernyms, relationQueries, outcastQueries, Output1, Output2, "case1_100_100");
 
             Console.WriteLine("case1_100_100 Completed :)");
             Console.WriteLine("------------------------------------");
@@ -379,59 +379,6 @@ namespace wordNet_project
             #endregion
             #region case2_1000_500
             Console.WriteLine("Running case2_1000_500.................");
-            //loading data from files
-             synsets = new List<string>();
-            synsets = ReadFromFile.Read_From_File(@"\Testcases\Complete\1-Small\Case2_1000_500\Input\1synsets.txt");
-
-            hypernyms = new List<string>();
-            hypernyms = ReadFromFile.Read_From_File(@"\Testcases\\Complete\1-Small\Case2_1000_500\Input\\2hypernyms.txt");
-
-            relationQueries = new List<string>();
-            relationQueries = ReadFromFile.Read_From_File(@"\Testcases\\Complete\1-Small\Case2_1000_500\Input\\3RelationsQueries.txt");
-
-            outcastQueries = new List<string>();
-            outcastQueries = ReadFromFile.Read_From_File(@"\Testcases\\Complete\1-Small\Case2_1000_500\Input\\4OutcastQueries.txt");
-            // loading output data from files
-            Output1 = new List<string>();
-            Output1 = ReadFromFile.Read_From_File(@"\Testcases\\Complete\1-Small\Case2_1000_500\\Output\\Output1.txt");
-
-            Output2 = new List<string>();
-            Output2 = ReadFromFile.Read_From_File(@"\Testcases\\Complete\1-Small\Case2_1000_500\\Output\\Output2.txt");
-            //----------------------------------------------------------
-            flag = Testing(true, synsets, hypernyms, relationQueries, outcastQueries, Output1, Output2, "case2_1000_500");
-
-            Console.WriteLine("case2_1000_500 Completed :)");
-            Console.WriteLine("------------------------------------");
-            #endregion
-           
-            #endregion
-
-            #region Medium
-           #region Case1_10000_5000
-            // bool flag = false;
-            //----------------------------------------------------------
-            Console.WriteLine("Running Case1_10000_5000.................");
-            //loading data from files
-            synsets = ReadFromFile.Read_From_File(@"\Testcases\Complete\2-Medium\Case1_10000_5000\Input\1synsets.txt");
-
-            hypernyms = ReadFromFile.Read_From_File(@"\Testcases\\Complete\2-Medium\Case1_10000_5000\Input\\2hypernyms.txt");
-
-            relationQueries = ReadFromFile.Read_From_File(@"\Testcases\\Complete\2-Medium\Case1_10000_5000\Input\\3RelationsQueries.txt");
-
-            outcastQueries = ReadFromFile.Read_From_File(@"\Testcases\\Complete\2-Medium\Case1_10000_5000\Input\\4OutcastQueries.txt");
-            // loading output data from files
-            Output1 = ReadFromFile.Read_From_File(@"\Testcases\\Complete\2-Medium\Case1_10000_5000\\Output\\Output1.txt");
-
-            Output2 = ReadFromFile.Read_From_File(@"\Testcases\\Complete\2-Medium\Case1_10000_5000\\Output\\Output2.txt");
-            //----------------------------------------------------------
-             flag = Testing(true, synsets, hypernyms, relationQueries, outcastQueries, Output1, Output2, "Case1_10000_5000");
-
-            Console.WriteLine("Test Case 1 Completed :)");
-            Console.WriteLine("------------------------------------");
-
-            #endregion
-            #region case2_1000_500
-            Console.WriteLine("Running TestCase 2.................");
             //loading data from files
             synsets = new List<string>();
             synsets = ReadFromFile.Read_From_File(@"\Testcases\Complete\1-Small\Case2_1000_500\Input\1synsets.txt");
@@ -451,17 +398,141 @@ namespace wordNet_project
             Output2 = new List<string>();
             Output2 = ReadFromFile.Read_From_File(@"\Testcases\\Complete\1-Small\Case2_1000_500\\Output\\Output2.txt");
             //----------------------------------------------------------
-            flag = Testing(true, synsets, hypernyms, relationQueries, outcastQueries, Output1, Output2, "TestCase 2");
+            flag = Testing_complet(true, synsets, hypernyms, relationQueries, outcastQueries, Output1, Output2, "case2_1000_500");
 
-            Console.WriteLine("Test Case 1 Completed :)");
+            Console.WriteLine("case2_1000_500 Completed :)");
             Console.WriteLine("------------------------------------");
             #endregion
+
+            #endregion
+            Console.WriteLine("Medium");
+            #region Medium
+
+            #region Case1_10000_5000
+            // bool flag = false;
+            //----------------------------------------------------------
+            Console.WriteLine("Running Medium Case1_10000_5000.................");
+            //loading data from files
+            synsets = ReadFromFile.Read_From_File(@"\Testcases\Complete\2-Medium\Case1_10000_5000\Input\1synsets.txt");
+
+            hypernyms = ReadFromFile.Read_From_File(@"\Testcases\\Complete\2-Medium\Case1_10000_5000\Input\\2hypernyms.txt");
+
+            relationQueries = ReadFromFile.Read_From_File(@"\Testcases\\Complete\2-Medium\Case1_10000_5000\Input\\3RelationsQueries.txt");
+
+            outcastQueries = ReadFromFile.Read_From_File(@"\Testcases\\Complete\2-Medium\Case1_10000_5000\Input\\4OutcastQueries.txt");
+            // loading output data from files
+            Output1 = ReadFromFile.Read_From_File(@"\Testcases\\Complete\2-Medium\Case1_10000_5000\\Output\\Output1.txt");
+
+            Output2 = ReadFromFile.Read_From_File(@"\Testcases\\Complete\2-Medium\Case1_10000_5000\\Output\\Output2.txt");
+            //----------------------------------------------------------
+            flag = Testing_complet(true, synsets, hypernyms, relationQueries, outcastQueries, Output1, Output2, "Case1_10000_5000");
+
+            Console.WriteLine("Case1_10000_5000 Completed :)");
+            Console.WriteLine("------------------------------------");
+
+            #endregion
+            #region Case2_10000_50000
+            Console.WriteLine("Running Case2_10000_50000.................");
+            //loading data from files
+            synsets = new List<string>();
+            synsets = ReadFromFile.Read_From_File(@"\Testcases\Complete\2-Medium\Case2_10000_50000\Input\1synsets.txt");
+
+            hypernyms = new List<string>();
+            hypernyms = ReadFromFile.Read_From_File(@"\Testcases\\Complete\2-Medium\Case2_10000_50000\Input\\2hypernyms.txt");
+
+            relationQueries = new List<string>();
+            relationQueries = ReadFromFile.Read_From_File(@"\Testcases\\Complete\2-Medium\Case2_10000_50000\Input\\3RelationsQueries.txt");
+
+            outcastQueries = new List<string>();
+            outcastQueries = ReadFromFile.Read_From_File(@"\Testcases\\Complete\2-Medium\Case2_10000_50000\Input\\4OutcastQueries.txt");
+            // loading output data from files
+            Output1 = new List<string>();
+            Output1 = ReadFromFile.Read_From_File(@"\Testcases\\Complete\2-Medium\Case2_10000_50000\\Output\\Output1.txt");
+
+            Output2 = new List<string>();
+            Output2 = ReadFromFile.Read_From_File(@"\Testcases\\Complete\2-Medium\Case2_10000_50000\\Output\\Output2.txt");
+            //----------------------------------------------------------
+            flag = Testing_complet(true, synsets, hypernyms, relationQueries, outcastQueries, Output1, Output2, "Case2_10000_50000");
+
+            Console.WriteLine("Case2_10000_50000 Completed :)");
+            Console.WriteLine("------------------------------------");
             #endregion
 
+            #endregion
+            Console.WriteLine("Larg");
             #region Larg
 
+            #region Case1_82K_100K_5000RQ
+            // bool flag = false;
+            //----------------------------------------------------------
+            Console.WriteLine("Running Case1_82K_100K_5000RQ.................");
+            //loading data from files
+            synsets = ReadFromFile.Read_From_File(@"\Testcases\Complete\3-Large\Case1_82K_100K_5000RQ\Input\1synsets.txt");
+
+            hypernyms = ReadFromFile.Read_From_File(@"\Testcases\\Complete\3-Large\Case1_82K_100K_5000RQ\Input\\2hypernyms.txt");
+
+            relationQueries = ReadFromFile.Read_From_File(@"\Testcases\\Complete\3-Large\Case1_82K_100K_5000RQ\Input\\3RelationsQueries.txt");
+
+            outcastQueries = ReadFromFile.Read_From_File(@"\Testcases\\Complete\3-Large\Case1_82K_100K_5000RQ\Input\\4OutcastQueries.txt");
+            // loading output data from files
+            Output1 = ReadFromFile.Read_From_File(@"\Testcases\\Complete\3-Large\Case1_82K_100K_5000RQ\\Output\\Output1.txt");
+
+            Output2 = ReadFromFile.Read_From_File(@"\Testcases\\Complete\3-Large\Case1_82K_100K_5000RQ\\Output\\Output2.txt");
+            //----------------------------------------------------------
+            flag = Testing_complet(true, synsets, hypernyms, relationQueries, outcastQueries, Output1, Output2, "Case1_82K_100K_5000RQ");
+
+            Console.WriteLine("Case1_82K_100K_5000RQ Completed :)");
+            Console.WriteLine("------------------------------------");
+
+            #endregion
+            #region Case2_82K_300K_1500RQ
+            // bool flag = false;
+            //----------------------------------------------------------
+            Console.WriteLine("Running Case2_82K_300K_1500RQ.................");
+            //loading data from files
+            synsets = ReadFromFile.Read_From_File(@"\Testcases\Complete\3-Large\Case2_82K_300K_1500RQ\Input\1synsets.txt");
+
+            hypernyms = ReadFromFile.Read_From_File(@"\Testcases\\Complete\3-Large\Case2_82K_300K_1500RQ\Input\\2hypernyms.txt");
+
+            relationQueries = ReadFromFile.Read_From_File(@"\Testcases\\Complete\3-Large\Case2_82K_300K_1500RQ\Input\\3RelationsQueries.txt");
+
+            outcastQueries = ReadFromFile.Read_From_File(@"\Testcases\\Complete\3-Large\Case2_82K_300K_1500RQ\Input\\4OutcastQueries.txt");
+            // loading output data from files
+            Output1 = ReadFromFile.Read_From_File(@"\Testcases\\Complete\3-Large\Case2_82K_300K_1500RQ\\Output\\Output1.txt");
+
+            Output2 = ReadFromFile.Read_From_File(@"\Testcases\\Complete\3-Large\Case2_82K_300K_1500RQ\\Output\\Output2.txt");
+            //----------------------------------------------------------
+            flag = Testing_complet(true, synsets, hypernyms, relationQueries, outcastQueries, Output1, Output2, "Case2_82K_300K_1500RQ");
+
+            Console.WriteLine("Case2_82K_300K_1500RQ Completed :)");
+            Console.WriteLine("------------------------------------");
+
+            #endregion
+            #region Case3_82K_300K_5000RQ
+            // bool flag = false;
+            //----------------------------------------------------------
+            Console.WriteLine("Running Case3_82K_300K_5000RQ.................");
+            //loading data from files
+            synsets = ReadFromFile.Read_From_File(@"\Testcases\Complete\3-Large\Case3_82K_300K_5000RQ\Input\1synsets.txt");
+
+            hypernyms = ReadFromFile.Read_From_File(@"\Testcases\\Complete\3-Large\Case3_82K_300K_5000RQ\Input\\2hypernyms.txt");
+
+            relationQueries = ReadFromFile.Read_From_File(@"\Testcases\\Complete\3-Large\Case3_82K_300K_5000RQ\Input\\3RelationsQueries.txt");
+
+            // loading output data from files
+            Output1 = ReadFromFile.Read_From_File(@"\Testcases\\Complete\3-Large\Case3_82K_300K_5000RQ\\Output\\Output1.txt");
+
+
+            //----------------------------------------------------------
+            flag = Testing_complet(false, synsets, hypernyms, relationQueries, outcastQueries, Output1, Output2, "Case3_82K_300K_5000RQ");
+
+            Console.WriteLine("Case3_82K_300K_5000RQ Completed :)");
+            Console.WriteLine("------------------------------------");
+
+            #endregion
             #endregion
 
+            Console.WriteLine("Complet testcases Completed");
             Console.WriteLine("Complet testcases Completed");
         }
         public static void CompletTest_using_Efficient_SCA()
@@ -945,7 +1016,73 @@ namespace wordNet_project
             return flag;
 
         }
+        public static bool Testing_complet(bool isoutcastQueries, List<string> synsets, List<string> hypernyms,
+List<string> relationQueries, List<string> outcastQueries, List<string> Output1, List<string> Output2,
+String TestcaseName)
+        {
+            bool flag = true;
+            Graph_Construction_For_Efficient_SCA test_Graph = new Graph_Construction_For_Efficient_SCA(synsets, hypernyms);
+
+            int cases_type_1 = int.Parse(relationQueries[0]);
+
+            for (int i = 1; i <= cases_type_1; i++)
+            {
+                string[] Splited_Line = relationQueries[i].Split(',');
+                string[] output_one_line = Output1[i - 1].Split(',');
+                int _dis = 0, _SCA_ID = 0;
+
+                Distans_And_SCA_Un_Efficient(ref _dis, ref _SCA_ID, Splited_Line[0], Splited_Line[1], test_Graph);
+                bool fnd = false;
+                string[] output_simi_line = output_one_line[1].Split(" ");
+                foreach (string k in output_simi_line)
+                {
+                    List<int> output_noun_synsets = MappingNounToSynsetsIDs.Maping_Noun_To_SynsetsIDs(test_Graph.Words, k);
+                    foreach (int j in output_noun_synsets)
+                    {
+
+                        if (_SCA_ID == j) fnd = true;
+                    }
+                }
+                if (_dis != int.Parse(output_one_line[0]) || !fnd)
+                {
+                    Console.WriteLine("Wrong answer at test" + TestcaseName + "  number :" + i + "  FND_dis:" + _dis + ", E_dis:" + int.Parse(output_one_line[0]));
+                    Console.WriteLine("synset_ID:" + _SCA_ID);
+                    flag = false;
+                    // break;
+                }
+            }
+            if (isoutcastQueries)
+            {
+                int cases_type_2 = int.Parse(outcastQueries[0]);
+                for (int i = 1; i < cases_type_2; i++)
+                {
+                    string[] Splited_Line = outcastQueries[i].Split(',');
+                    string[] output_one_line = Output2[i - 1].Split(" ");
+                    bool cast_FND = false;
+                    string s_word = "";
+                    outcastQueries_Un_Efficient(ref s_word, Splited_Line, test_Graph);
+                    foreach (string l in output_one_line)
+                        if (l == s_word)
+                            cast_FND = true;
+
+
+
+
+                    if (cast_FND == false)
+                    {
+                        Console.WriteLine("Wrong answer at test 1 number :" + i + "  outcastQueries ");
+                        flag = false;
+                        // break;
+                    }
+                }
+            }
+            return flag;
+
+        }
+
+
     }
+
 
 
 }
